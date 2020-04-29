b7200228ed74d7cdcf042e28c082eede59562d92
Holub Anna <modelkaiphotographer@gmail.com>
2020-04-29 09:54:45 +0300

    create branch Lab2_pointers

--- main.c
+++ main.c
@@ -1,42 +1,30 @@
 #include "stm32f10x.h"
-#include "stm32f10x_gpio.h"
-#include "stm32f10x_rcc.h"
 
 void delay(int ms) {
 	for(int i = 0; i < 10000 * ms; i++) { }
 }
 
 int main(void) {
-	char mor[] = {'.', '.','.', '_','_','_', ' ', '.','.','.', '_','_','_', ' ', '.', '_'};
-
-	int length = sizeof(mor) / sizeof(char);
-	int *GPIO_C_CRH = (int*)(0x00100000);
-	int *GPIO_C_ODR = (int*)(0x00002000);
-	int *apb2enr = (int*)0x40021018;
-	int i = 0;
-
-	*apb2enr |= 0x0001C;
-	*GPIO_C_CRH = 0x00300000;
+	
+	int *GPIO_B_CRH = (int*) (0x40010c04);
+	int *GPIO_B_ODR = (int*) (0x40010C0C);
+	int *GPIO_C_CRH = (int*) (0x40011004);
+	int *GPIO_C_IDR = (int*) (0x40011008);
+	
+	int *apb2enr = (int*) 0x40021018;
+	*apb2enr = 0x00018;
+	
+	*GPIO_C_CRH = 0x30000000; 
+	*GPIO_B_CRH = 0x00000033;
 
 	while(1) {
-
-		char elem = mor[i % length];
-		if(elem == '.') {
-			*GPIO_C_ODR=0x00000100;
-			delay(150);
-		} else if(elem == '_') {
-			*GPIO_C_ODR=0x00000100;
-			delay(300);
-		} else if(elem == ' ') {
-			*GPIO_C_ODR=0x0000000;
-			delay(600);
+		if (*GPIO_C_IDR & 0x8000) {
+			*GPIO_B_ODR = 0x2000;     // for B13
+			delay(1000);
+			*GPIO_B_ODR = 0x00000000;
+			delay(1000);
+		} else {
+			*GPIO_B_ODR = 0x00000000;
 		}
-		
-		i++;
-
-		if(i % length == length - 1) {
-			delay(3000);
-			i = 0;
-		};
 	}
 }
