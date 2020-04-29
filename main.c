83ba8403973a2e36cb16c4facc4c8bc8b197c62b
Holub Anna <modelkaiphotographer@gmail.com>
2020-04-29 09:57:10 +0300

    create branch Lab2_drivers

--- main.c
+++ main.c
@@ -1,42 +1,56 @@
 #include "stm32f10x.h"
-#include "stm32f10x_gpio.h"
-#include "stm32f10x_rcc.h"
 
 void delay(int ms) {
 	for(int i = 0; i < 10000 * ms; i++) { }
 }
 
-int main(void) {
-	char mor[] = {'.', '.','.', '_','_','_', ' ', '.','.','.', '_','_','_', ' ', '.', '_'};
+void main(){
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+  /* Initialize LED which connected to PC15 and PC8 */
 
-	int length = sizeof(mor) / sizeof(char);
-	int *GPIO_C_CRH = (int*)(0x00100000);
-	int *GPIO_C_ODR = (int*)(0x00002000);
-	int *apb2enr = (int*)0x40021018;
-	int i = 0;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	
+	/* Configure the GPIO_LED pin */
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-	*apb2enr |= 0x0001C;
-	*GPIO_C_CRH = 0x00300000;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	 //GPIO_SetBits(GPIOC, GPIO_Pin_8); // Set C8 to High level ("1")
+	GPIO_ResetBits(GPIOB, GPIO_Pin_8);
+	
+	while (1) {
+ 
+    if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_15) != 0) {
+        /* Toggle LED which connected to PC8*/
+        GPIOC->ODR ^= GPIO_Pin_8; // Invert C8
+ 
+        /* delay */
+		delay(1000);
+        /* Toggle LED which connected to PC13*/
+        GPIOC->ODR ^= GPIO_Pin_8;
+ 
+        /* delay */
+		delay(1000);  }
+    else {        GPIO_SetBits(GPIOC, GPIO_Pin_8);   }
 
-	while(1) {
+		GPIO_SetBits(GPIOC, GPIO_Pin_8);   
+		delay(1000);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_8); 
+		delay(1000);
+		GPIOC->ODR&=~GPIO_Pin_8;
+		delay(1000);
+		GPIOC->ODR|=GPIO_Pin_8;
+		delay(1000);
 
-		char elem = mor[i % length];
-		if(elem == '.') {
-			*GPIO_C_ODR=0x00000100;
-			delay(150);
-		} else if(elem == '_') {
-			*GPIO_C_ODR=0x00000100;
-			delay(300);
-		} else if(elem == ' ') {
-			*GPIO_C_ODR=0x0000000;
-			delay(600);
-		}
-		
-		i++;
+  }
 
-		if(i % length == length - 1) {
-			delay(3000);
-			i = 0;
-		};
-	}
 }
